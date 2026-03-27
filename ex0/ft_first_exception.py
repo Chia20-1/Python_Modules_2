1aff599671ea9f3a2c8cd718e824506aea6d3de9
Chia20-1 <chiachun2024@gmail.com>
2026-03-27 20:29:23 +0800

    Fix wrapper function

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -4,23 +4,18 @@ def input_temperature(temp_str: str) -> int:
     return int(temp_str)
 
 
-def test_temperature(temp_str: str) -> None:
-    print(f"Input data is '{temp_str}'")
-    try:
-        temp: int = input_temperature(temp_str)
-        print(f"Temperature is now {temp}°C")
-    except ValueError as e:
-        print("Caught input_temperature error:", e)
-
-
-def ft_first_exception() -> None:
-    print("=== Garden Exception ===\n")
-    test_temperature("25")
-    print()
-    test_temperature("abc")
-    print()
-    print("All tests completed - program didn't crash!")
+def test_temperature() -> None:
+    print("=== Garden Temperature ===\n")
+    test_cases = ["25", "abc"]
+    for test in test_cases:
+        print(f"Input data is '{test}'")
+        try:
+            temp: int = input_temperature(test)
+            print(f"Temperature is now {temp}°C")
+        except ValueError as e:
+            print("Caught input_temperature error:", e)
+        print()
 
 
 if __name__ == "__main__":
-    ft_first_exception()
+    test_temperature()
